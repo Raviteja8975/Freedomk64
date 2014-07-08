95cf8dced1b1f7ef8a21a056c86c960596757eff
Mark Bundgus <bundgus@gmail.com>
2014-07-07 23:47:12 -0500

    has link error... 8092nd character lost

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include "mbed.h"
 
 DigitalOut myled(LED1);
@@ -8,5 +9,29 @@ int main() {
         wait(0.2);
         myled = 0;
         wait(0.2);
+    }
+}
+
+*/
+
+#include "mbed.h"
+#include "rtos.h"
+
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+
+void led2_thread(void const *args) {
+    while (true) {
+        led2 = !led2;
+        Thread::wait(1000);
+    }
+}
+
+int main() {
+    Thread thread(led2_thread);
+
+    while (true) {
+        led1 = !led1;
+        Thread::wait(500);
     }
 }
